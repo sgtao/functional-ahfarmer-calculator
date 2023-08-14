cf4567c19034895d4b0473237d9f207fc72cab51
Shogo Ogami <sg.tao.so@gmail.com>
2023-08-14 21:30:17 +0900

    update src for Button component, issue#09

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-// import { useState } from "react";
+import { useState } from 'react';
 import Display from './components/Display';
+import Button from './components/Button';
+import calculate from './logic/calculate';
 import './App.css';
 
 const App = () => {
   const appName = 'ahfarmer-calculator';
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = (buttonName) => {
+    setState(calculate(state, buttonName));
+  };
 
   return (
     <div className="App">
@@ -11,7 +22,10 @@ const App = () => {
         <h3>{appName}</h3>
       </header>
       <div className="component-app">
-        <Display value="-9876543210+/*%" />
+        <Display value={state.next || state.total || '0'} />
+        <Button clickHandler={handleClick} name="7" orange />
+        <Button clickHandler={handleClick} name="4" />
+        <Button clickHandler={handleClick} name="1" wide />
       </div>
     </div>
   );
